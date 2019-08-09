80c0ebfa2ac66543996020169c23f8cae02cc95d
kirito5572 <first43013@gmail.com>
2019-08-09 22:25:37 +0900

    Create GreenColor.bat

---
+++ GreenColor.bat
@@ -0,0 +1,22 @@
+@echo off
+@chcp 65001 1> NUL 2> NUL
+setlocal enableextensions enabledelayedexpansion
+title 리토봇 INFO 콘솔
+color 0F
+echo STARTING.....
+echo ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ 
+echo ┃┏━━━━┓      ┏━━━━━━━┓    ━━━┳━━━  ┃ 
+echo ┃┃    ┗━┓    ┃       ┃       ┃     ┃ 
+echo ┃┃      ┗┓   ┃       ┃       ┃     ┃ 
+echo ┃┃      ┏┛   ┃       ┃       ┃     ┃ 
+echo ┃┃    ┏━┛    ┃       ┃       ┃     ┃ 
+echo ┃┣━━━━┫      ┃       ┃       ┃     ┃ 
+echo ┃┃    ┗━┓    ┃       ┃       ┃     ┃ 
+echo ┃┃      ┗┓   ┃       ┃       ┃     ┃ 
+echo ┃┃      ┏┛   ┃       ┃       ┃     ┃ 
+echo ┃┃    ┏━┛    ┃       ┃       ┃     ┃ 
+echo ┃┗━━━━┛      ┗━━━━━━━┛       ┃     ┃ 
+echo ┃                                  ┃ 
+echo ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ 
+java -jar C:\Users\Administrator\Documents\GitHub\DiscordBOT\build\libs\rito-bot-1.3.3-all.jar
+@echo on
