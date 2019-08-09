f5d3e3bd2acfc26113ffbe744730b644a0d976e5
cyd100 <34261355+cyd100@users.noreply.github.com>
2019-08-09 22:18:34 +0900

    Revert "Ver 1.3.4 RC1"
    This reverts commit 222676262813480d2c832e9130d70e86795525e6.

--- src/main/java/BOT/Commands/VersionCommand.java
+++ src/main/java/BOT/Commands/VersionCommand.java
@@ -10,15 +10,15 @@ public class VersionCommand implements ICommand {
     @Override
     public void handle(List<String> args, GuildMessageReceivedEvent event) {
         if(App.isDEBUG_MODE() || App.isONLINE_DEBUG()) {
-            event.getChannel().sendMessage("빌드 버젼 V 1.3.4 RC2(" + App.getTime() + ")").queue();
+            event.getChannel().sendMessage("빌드 버젼 V 1.3.4 (" + App.getTime() + ")").queue();
             return;
         }
-        event.getChannel().sendMessage("빌드 버젼 V 1.3.4 RC1 (" + App.getTime() + ")").queue();
+        event.getChannel().sendMessage("빌드 버젼 V 1.3.3 (" + App.getTime() + ")").queue();
         if(event.getGuild().getId().equals("508913681279483913")) {
             event.getChannel().sendMessage("봇 사이트: ``http://ritobot.mystrikingly.com``").queue();
             event.getChannel().sendMessage("업데이트 내역: \n" +
                     "```" +
-                    " - 업타임 커맨드 버그 수정\n" +
+                    " - 필터링 기능의 다운 문제 해결\n" +
                     " - 봇 본인의 필터링 비활성화\n" +
                     " - 필터링 데이터베이스 추가" +
                     "```").queue();
