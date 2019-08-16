2c895b98026d71c3782389dbcb415a51efe7be9c
kirito5572 <first43013@gmail.com>
2019-08-16 19:33:17 +0900

    Ver 1.3.5 Preview 4

--- GreenColor.bat
+++ GreenColor.bat
@@ -4,19 +4,21 @@ setlocal enableextensions enabledelayedexpansion
 title 리토봇 INFO 콘솔
 color 0F
 echo STARTING.....
-echo ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ 
-echo ┃┏━━━━┓      ┏━━━━━━━┓    ━━━┳━━━  ┃ 
-echo ┃┃    ┗━┓    ┃       ┃       ┃     ┃ 
-echo ┃┃      ┗┓   ┃       ┃       ┃     ┃ 
-echo ┃┃      ┏┛   ┃       ┃       ┃     ┃ 
-echo ┃┃    ┏━┛    ┃       ┃       ┃     ┃ 
-echo ┃┣━━━━┫      ┃       ┃       ┃     ┃ 
-echo ┃┃    ┗━┓    ┃       ┃       ┃     ┃ 
-echo ┃┃      ┗┓   ┃       ┃       ┃     ┃ 
-echo ┃┃      ┏┛   ┃       ┃       ┃     ┃ 
-echo ┃┃    ┏━┛    ┃       ┃       ┃     ┃ 
-echo ┃┗━━━━┛      ┗━━━━━━━┛       ┃     ┃ 
-echo ┃                                  ┃ 
-echo ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ 
-java -jar C:\Users\Administrator\Documents\GitHub\DiscordBOT\build\libs\rito-bot-1.3.3-all.jar
+echo ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ 
+echo ┃□□□□□□□□□□□□□□□□□□□□□□□□┃ 
+echo ┃□■■■■■□□□□□■■□□□■■■■■■■□┃ 
+echo ┃□■□□□□■□□□■□□■□□□□□■□□□□┃ 
+echo ┃□■□□□□□■□■□□□□■□□□□■□□□□┃ 
+echo ┃□■□□□□□■□■□□□□■□□□□■□□□□┃ 
+echo ┃□■□□□□■□□■□□□□■□□□□■□□□□┃ 
+echo ┃□■■■■■□□□■□□□□■□□□□■□□□□┃ 
+echo ┃□■□□□□■□□■□□□□■□□□□■□□□□┃ 
+echo ┃□■□□□□□■□■□□□□■□□□□■□□□□┃ 
+echo ┃□■□□□□□■□■□□□□■□□□□■□□□□┃ 
+echo ┃□■□□□□■□□□■□□■□□□□□■□□□□┃ 
+echo ┃□■■■■■□□□□□■■□□□□□□■□□□□┃ 
+echo ┃□□□□□□□□□□□□□□□□□□□□□□□□┃ 
+echo ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ 
+echo BOOTING.... 
+java -jar C:\Users\Administrator\Documents\GitHub\DiscordBOT\build\libs\rito-bot-1.3.x-all.jar
 @echo on
--- src/main/java/BOT/Commands/VersionCommand.java
+++ src/main/java/BOT/Commands/VersionCommand.java
@@ -10,15 +10,16 @@ public class VersionCommand implements ICommand {
     @Override
     public void handle(List<String> args, GuildMessageReceivedEvent event) {
         if(App.isDEBUG_MODE() || App.isONLINE_DEBUG()) {
-            event.getChannel().sendMessage("빌드 버젼 V 1.3.5 RC4(" + App.getTime() + ")").queue();
+            event.getChannel().sendMessage("빌드 버젼 V 1.3.5 Preview 5(" + App.getTime() + ")").queue();
             return;
         }
-        event.getChannel().sendMessage("빌드 버젼 V 1.3.5 RC3(" + App.getTime() + ")").queue();
+        event.getChannel().sendMessage("빌드 버젼 V 1.3.5 Preview 4(" + App.getTime() + ")").queue();
         if(event.getGuild().getId().equals("508913681279483913")) {
             event.getChannel().sendMessage("봇 사이트: ``http://ritobot.mystrikingly.com``").queue();
             event.getChannel().sendMessage("업데이트 내역: \n" +
                     "```" +
-                    " - 서버 커스텀 시스템 서버밴 기능 추가\n" +
+                    " - .jar 파일 구동 .bat 머신 파일 변경\n" +
+                    " - 버젼 정보를 RC 에서 Preview로 변경\n" +
                     " - 필터링 데이터베이스 추가" +
                     "```").queue();
         }
