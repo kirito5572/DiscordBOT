dc61c343be68208d6c43618840dc32c4ec9f9ee8
kirito5572 <first43013@gmail.com>
2019-09-11 08:25:45 +0900

    update

--- src/main/java/BOT/Commands/CatCommand.java
+++ src/main/java/BOT/Commands/CatCommand.java
@@ -1,13 +1,15 @@
 package BOT.Commands;
 
 import BOT.Constants;
-import BOT.Music.PlayerManager;
 import BOT.Objects.ICommand;
 import me.duncte123.botcommons.messaging.EmbedUtils;
 import me.duncte123.botcommons.web.WebUtils;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.Permission;
-import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.PrivateChannel;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 import java.io.File;
@@ -24,87 +26,99 @@ public class CatCommand implements ICommand {
     public void handle(List<String> args, GuildMessageReceivedEvent event) {
 
         Member selfMember = event.getGuild().getSelfMember();
+        int num = 1;
+        try {
+            num = Integer.parseInt(args.get(0));
+        } catch (Exception ignored) {
+
+        }
+        if(num < 10) {
+            event.getChannel().sendMessage("최대 전송 가능량은 10개입니다.").queue();
+
+            return;
+        }
 
         if(!selfMember.hasPermission(Permission.MESSAGE_EMBED_LINKS)) {
             event.getChannel().sendMessage("봇이 링크 메세지를 보낼 권한이 없습니다.").queue();
 
             return;
         }
+        for(int i = 0; i < num; i++) {
+            WebUtils.ins.scrapeWebPage("https://nekos.life/").async((document) -> {
+                String a = document.getElementsByTag("head").first().toString();
+                int b = a.indexOf("meta property=\"og:url\" content=\"");
+                int c = a.indexOf("<meta property=\"og:image\" content=\"");
+                a = a.substring(b + 32, c - 5);
+                EmbedBuilder embed = EmbedUtils.embedImage(a);
+                TextChannel channel = event.getChannel();
+                Message message;
 
-        WebUtils.ins.scrapeWebPage("https://nekos.life/").async((document) -> {
-            String a = document.getElementsByTag("head").first().toString();
-            int b = a.indexOf("meta property=\"og:url\" content=\"");
-            int c = a.indexOf("<meta property=\"og:image\" content=\"");
-            a = a.substring(b + 32, c - 5);
-            EmbedBuilder embed = EmbedUtils.embedImage(a);
-            TextChannel channel = event.getChannel();
-            Message message;
+                if (event.getGuild().getId().equals("600010501266866186")) {
+                    try {
+                        message = channel.sendMessage("*주의 이 커맨드는 네다씹 커맨드입니다*." + "\n" +
+                                "실행을 원하시면 :one: 아니면 :two:").complete();
 
-            if(event.getGuild().getId().equals("600010501266866186")) {
-                try {
-                    message = channel.sendMessage("*주의 이 커맨드는 네다씹 커맨드입니다*." + "\n" +
-                            "실행을 원하시면 :one: 아니면 :two:").complete();
+                        Thread.sleep(400);
+                        String ID = channel.getLatestMessageId();
 
-                    Thread.sleep(400);
-                    String ID = channel.getLatestMessageId();
+                        channel.addReactionById(ID, "1\u20E3").queue();
+                        channel.addReactionById(ID, "2\u20E3").queue();
+                        Thread.sleep(500);
 
-                    channel.addReactionById(ID, "1\u20E3").queue();
-                    channel.addReactionById(ID, "2\u20E3").queue();
-                    Thread.sleep(500);
+                        for (int j = 0; j < 11; j++) {
+                            Thread.sleep(1000);
+                            System.out.println(channel.getMessageById(ID).complete().getReactions().get(0).getReactionEmote().getEmote());
+                            if (channel.getMessageById(ID).complete().getReactions().get(0).getCount() == 2) {
+                                Random random = new Random();
+                                PrivateChannel channel1;
+                                channel1 = event.getAuthor().openPrivateChannel().complete();
+                                if (random.nextInt(100) > 5) { //95%
+                                    channel1.sendMessage(embed.build()).queue();
+                                    message.delete().queue();
+                                } else { //5%
+                                    Random fileRandom = new Random();
+                                    int intTemp = fileRandom.nextInt(20);
+                                    InputStream inputStream = this.getClass().getResourceAsStream("/" + "lol" + intTemp + ".jpg");
+                                    File file;
+                                    try {
+                                        file = convertInputStreamToFile(inputStream, ".jpg");
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
 
-                    for (int i = 0; i < 11; i++) {
-                        Thread.sleep(1000);
-                        System.out.println(channel.getMessageById(ID).complete().getReactions().get(0).getReactionEmote().getEmote());
-                        if (channel.getMessageById(ID).complete().getReactions().get(0).getCount() == 2) {
-                            Random random = new Random();
-                            PrivateChannel channel1;
-                            channel1 = event.getAuthor().openPrivateChannel().complete();
-                            if(random.nextInt(100) > 5) { //95%
-                                channel1.sendMessage(embed.build()).queue();
-                                message.delete().queue();
-                            } else { //5%
-                                Random fileRandom = new Random();
-                                int intTemp = fileRandom.nextInt(6);
-                                InputStream inputStream = this.getClass().getResourceAsStream("/" + "lol" + intTemp +".jpg");
-                                File file;
-                                try {
-                                    file = convertInputStreamToFile(inputStream, ".jpg");
-                                } catch (IOException e) {
-                                    e.printStackTrace();
+                                        event.getChannel().sendMessage("에러가 발생했습니다.").queue();
 
-                                    event.getChannel().sendMessage("에러가 발생했습니다.").queue();
-
-                                    return;
+                                        return;
+                                    }
+                                    channel1.sendFile(file).queue();
+                                    message.delete().queue();
                                 }
-                                channel1.sendFile(file).queue();
-                                message.delete().queue();
+
+                                return;
+                            } else if (channel.getMessageById(ID).complete().getReactions().get(1).getCount() == 2) {
+                                message.delete().complete();
+                                message = event.getChannel().sendMessage("전송이 취소되었습니다.").complete();
+                                message.delete().queueAfter(5, TimeUnit.SECONDS);
+                                return;
                             }
-
-                            return;
-                        } else if (channel.getMessageById(ID).complete().getReactions().get(1).getCount() == 2) {
-                            message.delete().complete();
-                            message = event.getChannel().sendMessage("전송이 취소되었습니다.").complete();
-                            message.delete().queueAfter(5, TimeUnit.SECONDS);
-                            return;
                         }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+                    message = channel.sendMessage("대기 시간이 초과되어 전송이 취소되었습니다.").complete();
+                    message.delete().queueAfter(5, TimeUnit.SECONDS);
+
+                } else {
+                    event.getChannel().sendMessage(embed.build()).queue();
                 }
-
-                message = channel.sendMessage("대기 시간이 초과되어 전송이 취소되었습니다.").complete();
-                message.delete().queueAfter(5, TimeUnit.SECONDS);
-
-            } else {
-                event.getChannel().sendMessage(embed.build()).queue();
-            }
-        });
+            });
+        }
     }
 
     @Override
     public String getHelp() {
         return "랜덤 네코 생성기 \n" +
-                "사용법: `" + Constants.PREFIX + getInvoke() + "`";
+                "사용법: `" + Constants.PREFIX + getInvoke() + "` <반복숫자>(미입력시 1)";
     }
 
     @Override
