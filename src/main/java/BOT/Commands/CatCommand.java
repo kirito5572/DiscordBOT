535ec76b3ac5ce73262d22815a4466d215cd3b7f
kirito5572 <first43013@gmail.com>
2019-09-11 08:35:16 +0900

    Ver 1.4.5 Preview 8

--- src/main/java/BOT/Commands/CatCommand.java
+++ src/main/java/BOT/Commands/CatCommand.java
@@ -43,6 +43,52 @@ public class CatCommand implements ICommand {
 
             return;
         }
+        TextChannel channel = event.getChannel();
+        Message message;
+        message = channel.sendMessage("*주의 이 커맨드는 네다씹 커맨드입니다*." + "\n" +
+                "실행을 원하시면 :one: 아니면 :two:").complete();
+
+        try {
+            Thread.sleep(400);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String ID = channel.getLatestMessageId();
+
+        channel.addReactionById(ID, "1\u20E3").queue();
+        channel.addReactionById(ID, "2\u20E3").queue();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if(event.getGuild().getId().equals("600010501266866186")) {
+            boolean flag = true;
+
+            for (int j = 0; j < 11; j++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(channel.getMessageById(ID).complete().getReactions().get(0).getReactionEmote().getEmote());
+                if (channel.getMessageById(ID).complete().getReactions().get(0).getCount() == 2) {
+                    flag = false;
+                } else if (channel.getMessageById(ID).complete().getReactions().get(1).getCount() == 2) {
+                    message.delete().complete();
+                    message = event.getChannel().sendMessage("전송이 취소되었습니다.").complete();
+                    message.delete().queueAfter(5, TimeUnit.SECONDS);
+                    return;
+                }
+            }
+            if (flag) {
+                message.delete().queue();
+                message = channel.sendMessage("대기 시간이 초과되어 전송이 취소되었습니다.").complete();
+                message.delete().queueAfter(5, TimeUnit.SECONDS);
+
+                return;
+            }
+        }
         for(int i = 0; i < num; i++) {
             WebUtils.ins.scrapeWebPage("https://nekos.life/").async((document) -> {
                 String a = document.getElementsByTag("head").first().toString();
@@ -50,63 +96,29 @@ public class CatCommand implements ICommand {
                 int c = a.indexOf("<meta property=\"og:image\" content=\"");
                 a = a.substring(b + 32, c - 5);
                 EmbedBuilder embed = EmbedUtils.embedImage(a);
-                TextChannel channel = event.getChannel();
-                Message message;
 
                 if (event.getGuild().getId().equals("600010501266866186")) {
-                    try {
-                        message = channel.sendMessage("*주의 이 커맨드는 네다씹 커맨드입니다*." + "\n" +
-                                "실행을 원하시면 :one: 아니면 :two:").complete();
+                    Random random = new Random();
+                    PrivateChannel channel1;
+                    channel1 = event.getAuthor().openPrivateChannel().complete();
+                    if (random.nextInt(100) > 5) { //95%
+                        channel1.sendMessage(embed.build()).queue();
+                    } else { //5%
+                        Random fileRandom = new Random();
+                        int intTemp = fileRandom.nextInt(20);
+                        InputStream inputStream = this.getClass().getResourceAsStream("/" + "lol" + intTemp + ".jpg");
+                        File file;
+                        try {
+                            file = convertInputStreamToFile(inputStream, ".jpg");
+                        } catch (IOException e) {
+                            e.printStackTrace();
 
-                        Thread.sleep(400);
-                        String ID = channel.getLatestMessageId();
+                            event.getChannel().sendMessage("에러가 발생했습니다.").queue();
 
-                        channel.addReactionById(ID, "1\u20E3").queue();
-                        channel.addReactionById(ID, "2\u20E3").queue();
-                        Thread.sleep(500);
-
-                        for (int j = 0; j < 11; j++) {
-                            Thread.sleep(1000);
-                            System.out.println(channel.getMessageById(ID).complete().getReactions().get(0).getReactionEmote().getEmote());
-                            if (channel.getMessageById(ID).complete().getReactions().get(0).getCount() == 2) {
-                                Random random = new Random();
-                                PrivateChannel channel1;
-                                channel1 = event.getAuthor().openPrivateChannel().complete();
-                                if (random.nextInt(100) > 5) { //95%
-                                    channel1.sendMessage(embed.build()).queue();
-                                    message.delete().queue();
-                                } else { //5%
-                                    Random fileRandom = new Random();
-                                    int intTemp = fileRandom.nextInt(20);
-                                    InputStream inputStream = this.getClass().getResourceAsStream("/" + "lol" + intTemp + ".jpg");
-                                    File file;
-                                    try {
-                                        file = convertInputStreamToFile(inputStream, ".jpg");
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-
-                                        event.getChannel().sendMessage("에러가 발생했습니다.").queue();
-
-                                        return;
-                                    }
-                                    channel1.sendFile(file).queue();
-                                    message.delete().queue();
-                                }
-
-                                return;
-                            } else if (channel.getMessageById(ID).complete().getReactions().get(1).getCount() == 2) {
-                                message.delete().complete();
-                                message = event.getChannel().sendMessage("전송이 취소되었습니다.").complete();
-                                message.delete().queueAfter(5, TimeUnit.SECONDS);
-                                return;
-                            }
+                            return;
                         }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        channel1.sendFile(file).queue();
                     }
-
-                    message = channel.sendMessage("대기 시간이 초과되어 전송이 취소되었습니다.").complete();
-                    message.delete().queueAfter(5, TimeUnit.SECONDS);
 
                 } else {
                     event.getChannel().sendMessage(embed.build()).queue();
