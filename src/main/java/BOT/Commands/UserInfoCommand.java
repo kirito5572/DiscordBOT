bf11eaedab642432f584ff6babfde3292ae768bc
kirito5572 <first43013@gmail.com>
2019-09-29 15:13:07 +0900

    update

--- src/main/java/BOT/Commands/UserInfoCommand.java
+++ src/main/java/BOT/Commands/UserInfoCommand.java
@@ -10,6 +10,7 @@ import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,26 +18,27 @@ import java.util.stream.Collectors;
 public class UserInfoCommand implements ICommand {
     @Override
     public void handle(List<String> args, GuildMessageReceivedEvent event) {
+        User user;
+        Member member;
         if(args.isEmpty()) {
-            event.getChannel().sendMessage("인수 부족 '" + App.getPREFIX() + "help" +
-                    getInvoke() + "'").queue();
-            return;
-        }
+            user = event.getMember().getUser();
+            member = event.getMember();
+        } else {
+            String joined = String.join(" ", args);
+            List<User> foundUsers = FinderUtil.findUsers(joined, event.getGuild().getJDA());
 
-        String joined = String.join(" ", args);
-        List<User> foundUsers = FinderUtil.findUsers(joined, event.getGuild().getJDA());
+            if (foundUsers.isEmpty()) {
+                List<Member> foundMember = FinderUtil.findMembers(joined, event.getGuild());
+                if (foundMember.isEmpty()) {
+                    event.getChannel().sendMessage("'" + joined + "' 라는 유저는 없습니다.").queue();
+                    return;
+                }
 
-        if(foundUsers.isEmpty()) {
-            List<Member> foundMember = FinderUtil.findMembers(joined, event.getGuild());
-            if(foundMember.isEmpty()) {
-                event.getChannel().sendMessage("'" + joined + "' 라는 유저는 없습니다.").queue();
-                return;
+                foundUsers = foundMember.stream().map(Member::getUser).collect(Collectors.toList());
             }
-
-            foundUsers = foundMember.stream().map(Member::getUser).collect(Collectors.toList());
+            user = foundUsers.get(0);
+            member = event.getGuild().getMember(user);
         }
-        User user = foundUsers.get(0);
-        Member member = event.getGuild().getMember(user);
 
         MessageEmbed embed = EmbedUtils.defaultEmbed()
                 .setColor(member.getColor())
@@ -44,8 +46,8 @@ public class UserInfoCommand implements ICommand {
                 .addField("유저이름#번호", String.format("%#s", user), false)
                 .addField("표시 이름", member.getEffectiveName(), false)
                 .addField("유저 ID + 언급 멘션", String.format("%s (%s)", user.getId(), member.getAsMention()), false)
-                .addField("가입 일자", user.getCreationTime().format(DateTimeFormatter.RFC_1123_DATE_TIME), false)
-                .addField("서버 초대 일자", member.getJoinDate().format(DateTimeFormatter.RFC_1123_DATE_TIME), false)
+                .addField("가입 일자", user.getCreationTime().format(DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneId.systemDefault())), false)
+                .addField("서버 초대 일자", member.getJoinDate().format(DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneId.systemDefault())), false)
                 .addField("온라인 상태", member.getOnlineStatus().name().toLowerCase().replaceAll("_", " "), false)
                 .addField("봇 여부", user.isBot() ? "Yes" : "No", false)
                 .build();
