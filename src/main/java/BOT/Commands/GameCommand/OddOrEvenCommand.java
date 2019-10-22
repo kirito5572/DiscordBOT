2e4665d0797e522d3002a2a39ed34158b0d08b1a
kirito5572 <first43013@gmail.com>
2019-10-22 18:45:02 +0900

    I got a bug

--- src/main/java/BOT/Commands/GameCommand/OddOrEvenCommand.java
+++ src/main/java/BOT/Commands/GameCommand/OddOrEvenCommand.java
@@ -1,6 +1,8 @@
 package BOT.Commands.GameCommand;
 
 import BOT.Objects.ICommand;
+import me.duncte123.botcommons.messaging.EmbedUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 import java.util.List;
@@ -10,40 +12,64 @@ public class OddOrEvenCommand implements ICommand {
     private Random random = new Random();
     @Override
     public void handle(List<String> args, GuildMessageReceivedEvent event) {
-        event.getMessage().delete().queue();
-        event.getChannel().sendMessage("홀수 또는 짝수를 선택해주세요").queue();
-        boolean even = random.nextBoolean();
-        try {
-            for (int i = 0; i < 21; i++) {
-                Thread.sleep(500);
-                try {
-                    String message = event.getChannel().retrieveMessageById(event.getChannel().getLatestMessageId()).complete().getContentRaw();
-                    System.out.println(event.getChannel().getLatestMessageId());
-                    if(message.equals("홀수")) {
-                        if(even) {
-                            event.getChannel().sendMessage("성공!").queue();
+        new Thread(() -> {
+            event.getChannel().sendMessage("홀수 또는 짝수를 선택해주세요").queue();
+            boolean even = random.nextBoolean();
+            try {
+                int i;
+                for (i = 0; i < 21; i++) {
+                    Thread.sleep(500);
+                    try {
+                        String id = event.getChannel().getLatestMessageId();
+                        String message = event.getChannel().retrieveMessageById(id).complete().getContentRaw();
+                        String[] result = new String[] {
+                                "a", "b", "c"
+                        };
+                        if(message.equals("홀수")) {
+                            result[2] = "ok";
+                            if(even) {
+                                result[0] = "홀수";
+                                result[1] = "성공";
+                            } else {
+                                result[0] = "짝수";
+                                result[1] = "실패";
+                            }
+                            EmbedBuilder builder = EmbedUtils.defaultEmbed()
+                                    .setTitle("홀/짝")
+                                    .addField("결과", result[1], false)
+                                    .addField("추첨 결과", result[0], false)
+                                    .setFooter("과도한 도박은 정신건강에 해롭습니다");
+                            event.getChannel().sendMessage(builder.build()).queue();
                             break;
-                        } else {
-                            event.getChannel().sendMessage("실패!").queue();
+                        } else if (message.equals("짝수")) {
+                            result[2] = "ok";
+                            if(even) {
+                                result[0] = "홀수";
+                                result[1] = "실패";
+                            } else {
+                                result[0] = "짝수";
+                                result[1] = "성공";
+                            }
+                            EmbedBuilder builder = EmbedUtils.defaultEmbed()
+                                    .setTitle("홀/짝")
+                                    .addField("결과", result[1], false)
+                                    .addField("추첨 결과", result[0], false)
+                                    .setFooter("과도한 도박은 정신건강에 해롭습니다");
+                            event.getChannel().sendMessage(builder.build()).queue();
                             break;
                         }
-                    } else if (message.equals("짝수")) {
-                        if(even) {
-                            event.getChannel().sendMessage("실패").queue();
-                            break;
-                        } else {
-                            event.getChannel().sendMessage("성공!").queue();
-                            break;
-                        }
-                    }
 
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
+                if(i == 20) {
+                    event.getChannel().sendMessage("10초 동안 응답이 없어 명령어 사용이 취소되었습니다.").queue();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        }).start();
     }
 
     @Override
