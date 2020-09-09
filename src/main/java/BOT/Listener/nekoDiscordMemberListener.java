3a4a43fc5965bff5880d1884d2812ac7834cc813
kirito5572 <first43013@gmail.com>
2020-09-10 00:06:59 +0900

    codeAddForNekoDiscord

--- src/main/java/BOT/Listener/nekoDiscordMemberListener.java
+++ src/main/java/BOT/Listener/nekoDiscordMemberListener.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -14,15 +15,45 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Objects;
+import java.util.Random;
 
 public class nekoDiscordMemberListener extends ListenerAdapter {
     private final Logger logger = LoggerFactory.getLogger(nekoDiscordMemberListener.class);
     private static String Chating1;
+    private final String nekoDiscordServerId = "439780696999985172";
+    private final String[] nekoDiscordTeamRoleId = {
+            "439828127133466634", "439828232779333632", "439828312328765460",  // 지휘 / 정보 / 교육
+            "439828408684380180", "439828528758915073", "439828616814133249",  // 안전 / 중앙 / 징계
+            "439828703661391873", "439828771059531796", "439830502334136320",  // 복지 / 기록 / 추출
+            "619921319533740032"                                               // 설계
+    };
+    private final Random random = new Random();
+
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        Guild guild = event.getGuild();
+        if(guild.getId().equals(nekoDiscordServerId)) {
+            if(event.getMessage().getContentRaw().startsWith("!")) {
+                if(event.getMessage().getContentRaw().startsWith("!join")) {
+                    Role role = guild.getRoleById(nekoDiscordTeamRoleId[random.nextInt(10)]);
+                    assert role != null;
+                    guild.addRoleToMember(Objects.requireNonNull(event.getMember()), role).complete();
+                    try {
+                        event.getMember().getUser().openPrivateChannel().complete()
+                                .sendMessage("랜덤으로 팀이 배정되었습니다. 배정된 팀은 **\"" + role.getName() + "\"**").queue();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
     @Override
     public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
         Guild guild = event.getGuild();
-        if(guild.getId().equals("439780696999985172")) {
+        if(guild.getId().equals(nekoDiscordServerId)) {
             Role role = guild.getRoleById("625549604674600970");  //휴식자
             Role role2 = guild.getRoleById("619556312627544105");  //사무직
             Member member = event.getMember();
@@ -64,7 +95,7 @@ public class nekoDiscordMemberListener extends ListenerAdapter {
     }
     public void onMessageReactionRemove(@NotNull MessageReactionRemoveEvent event) {
         Guild guild = event.getGuild();
-        if(guild.getId().equals("439780696999985172")) {
+        if(guild.getId().equals(nekoDiscordServerId)) {
             Role role = guild.getRoleById("625549604674600970");  //휴식자
             Member member = event.getMember();
             if (event.getMessageId().equals(Chating1)) {
